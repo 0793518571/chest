824c76543aa36a1fbb3b7b1198c46a6217ef71ca
0793518571 <behradshayan93@gmail.com>
2024-08-20 13:03:54 +0330

    Add files via upload

---
+++ فایل بهراد.py
@@ -0,0 +1,8 @@
+password='bacheh ziba'
+for f in range(3):
+    guess=input('what is my password?: ')
+    if password==guess:
+        print('^_^ excellent')
+    else:
+        print('try again')
+    
